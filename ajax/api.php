3bf413d3cecd5bb437ea87f2015888f033e2f31e
Rony <ronyrodriguez2011@hotmail.com>
2019-08-23 15:06:30 -0600

    Commit Inicial

---
+++ ajax/api.php
@@ -0,0 +1,21 @@
+<?php
+
+    include("../class/class-Conexion.php");
+    
+
+    $conexion = new Conexion();
+
+    switch($_GET["accion"]){
+
+        case "''":
+        break;
+
+        case "''":
+        break;
+
+        case "''":
+        break;
+
+    }
+
+?>
